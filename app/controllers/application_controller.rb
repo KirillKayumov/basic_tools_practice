6ae55dce63eda3f614df7b714679703fe98b9fab
Kirill Kayumov <kirill.kayumov@flatstack.com>
2016-07-19 19:43:32 +0300

    Add Feedback form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
+require "application_responder"
+
 class ApplicationController < ActionController::Base
+  self.responder = ApplicationResponder
+
   protect_from_forgery with: :exception
+
+  responders :flash
+  respond_to :html
 end
