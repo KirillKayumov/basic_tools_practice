f1bf4fc7961dfb30525fdf3f2b142bbd026b95a3
Kirill Kayumov <kirill.kayumov@flatstack.com>
2016-07-20 11:25:47 +0300

    Remove :user attr from Feedback model

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,7 +1,7 @@
 class Feedback
   include ActiveModel::Model
 
-  attr_accessor :email, :name, :message, :user
+  attr_accessor :email, :name, :message
 
   validates :email, :name, :message, presence: true
   validates :email, format: /\A[^@]+@[^@]+\z/
